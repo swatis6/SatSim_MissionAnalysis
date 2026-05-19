b3ecd803ba9df598c44e61b07b1f53896646b2d2
Orlando LuPone <lupone.o@northeastern.edu>
2026-05-19 17:17:03 -0400

    Updated tests to use fixtures

--- tests/test_propagator.py
+++ tests/test_propagator.py
@@ -9,57 +9,67 @@ from satsim.spacecraft.spacecraft import Spacecraft
 from satsim.environment.environment import Environment
 from satsim.mission.mission import Mission
 
-class TestPropagator:
+def load_yaml(name):
+    with open("src/satsim/config/" + name, "r") as f:
+        return yaml.safe_load(f) or {}
 
-    # Sets up inital state for tests where J2 & drag are off.
-    @classmethod
-    def setup_class(cls):
-        cls.runSteps = 553
-        cls.dt = 10  # s
-        sc_config = {
-            "name": "TesterSat",
-            "initial_state": {
-                "r": [6.778137e6, 0, 0],  # 400 km altitude
-                "v": [0, 7672.6, 0]},  # v for cicular orbit at altitude
-            "mass_props": {
-                "mass": 4.0,
-                "cd": 2.2,
-                "area": 0.03}}
-        enviromentCong = {
-            "gravity": {
-                "use_j2": False},
-            "atmosphere": {
-                "rho0": 0,
-                "scale_height": 8500.0}}  # No J2 / No Drag
-        cls.sim = Simulator(Spacecraft(sc_config), Environment(enviromentCong), Mission(""))
-        cls.duration = cls.dt * cls.runSteps
+@pytest.fixture
+def dt():
+    return 10
 
-    # Testing that the altitude has almost not changed during the sim
-    def test_Altitude_Conservation(self):
-        self.sim.run(self.duration, self.dt)
-        alt = np.array(self.sim.history["altitude"]) / 1000.0
-        assert abs(alt[-1] - alt[0]) < 1
+@pytest.fixture
+def duration(dt):
+    runSteps = 553
+    return dt * runSteps
 
-    # Testing that the energy is almost hasn't changed during the sim
-    def test_energy_conservation(self):
-        self.sim.run(self.duration, self.dt)
-        pos = np.array(self.sim.history["r"])
-        posMag = np.linalg.norm(pos, axis=1)
-        vel = np.array(self.sim.history["v"])
-        velMag = np.linalg.norm(vel, axis=1)
-        energy = (1 / 2) * velMag ** 2 - (MU_EARTH / posMag)
-        assert abs(energy[-1] - energy[0]) / energy[0] == pytest.approx(0, abs=1e-8)
+@pytest.fixture
+def scConfig():
+    scConfig = load_yaml("spacecraft.yaml")
+    scConfig["initial_state"]["r"] = [6.778137e6, 0, 0]
+    scConfig["initial_state"]["v"] = [0, 7672.6, 0]
+    scConfig["mass_props"]["mass"] = 4.0
+    scConfig["mass_props"]["cd"] = 2.2
+    scConfig["mass_props"]["area"] = 0.03
+    return scConfig
 
-    # Testing that the magnitude and direction of the satelitte is the same at the beginning and end of sim
-    def test_angular_momentum_conservation(self):
-        self.sim.run(self.duration, self.dt)
-        pos = np.array(self.sim.history["r"])
-        vel = np.array(self.sim.history["v"])
-        h = np.cross(pos, vel)  # angular momentum
-        hCrossMag = np.linalg.norm(h, axis=1)
-        assert abs(hCrossMag[-1] - hCrossMag[0]) / hCrossMag[0] < 1e-10  # magnitude
+@pytest.fixture
+def enviromentConfig():
+    enviromentConfig = load_yaml("environment.yaml")
+    enviromentConfig["gravity"]["use_j2"] = False
+    enviromentConfig["atmosphere"]["rho0"] = 0  # No J2 / No Drag
+    enviromentConfig["atmosphere"]["scale_height"] = 8500.0
+    return enviromentConfig
 
-        hUnitVectorStart = h[0] / np.linalg.norm(h[0])
-        hUnitVectorEnd = h[-1] / np.linalg.norm(h[-1])
-        hCrossDot = np.dot(hUnitVectorStart, hUnitVectorEnd)
-        assert hCrossDot == pytest.approx(1, abs=1e-10)  # direction
+@pytest.fixture
+def sim(duration, dt, scConfig, enviromentConfig):
+    sim = Simulator(Spacecraft(scConfig), Environment(enviromentConfig), Mission(""))
+    sim.run(duration, dt)
+    alt = np.array(sim.history["altitude"]) / 1000.0 #km
+    pos = np.array(sim.history["r"])
+    vel = np.array(sim.history["v"])
+    return alt, pos, vel
+
+# Testing that the altitude has almost not changed during the sim
+def test_Altitude_Conservation(sim):
+    alt, pos, vel = sim
+    assert abs(alt[-1] - alt[0]) < 1
+
+# Testing that the energy is almost hasn't changed during the sim
+def test_energy_conservation(sim):
+    alt, pos, vel = sim
+    posMag = np.linalg.norm(pos, axis = 1)
+    velMag = np.linalg.norm(vel, axis = 1)
+    energy = (1 / 2) * velMag ** 2 - (MU_EARTH / posMag)
+    assert abs(energy[-1] - energy[0]) / energy[0] == pytest.approx(0, abs = 1e-8)
+
+# Testing that the magnitude and direction of the satelitte is the same at the beginning and end of sim
+def test_angular_momentum_conservation(sim):
+    alt, pos, vel = sim
+    h = np.cross(pos, vel)  # angular momentum
+    hCrossMag = np.linalg.norm(h, axis = 1)
+    assert abs(hCrossMag[-1] - hCrossMag[0]) / hCrossMag[0] < 1e-10  # magnitude
+
+    hUnitVectorStart = h[0] / np.linalg.norm(h[0])
+    hUnitVectorEnd = h[-1] / np.linalg.norm(h[-1])
+    hCrossDot = np.dot(hUnitVectorStart, hUnitVectorEnd)
+    assert hCrossDot == pytest.approx(1, abs = 1e-10)  # direction
